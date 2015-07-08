84baaa2d6cc6914c61cd396496543c406abc1363
Heather Booker <heather.booker@hotmail.com>
2015-07-07 17:35:22 -0700

    begin to apply flat ui

--- Scripts/Controller.js
+++ Scripts/Controller.js
@@ -1,3 +1,32 @@
 $("#menu-toggle").click(function() {
     $("#sidebar-wrapper").toggle();
 });
+
+
+
+
+function resizeNav() {
+    if ($(".navbar").height() > $('#godown').height()) {
+        $('#godown').height($(".navbar").height() + 10)
+    };
+}
+
+$(document).ready(function() {
+    $(resizeNav());
+});
+
+$(window).resize(function() {
+    $(resizeNav());
+})
+
+
+$(document).ready(function() {
+    $("select").selectpicker({
+        style: 'btn-primary',
+        menuStyle: 'dropdown-inverse'
+    });
+});
+
+$("select").select2({
+    ".mbl": 'dropdown-inverse'
+});
