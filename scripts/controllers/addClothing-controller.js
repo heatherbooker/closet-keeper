c1e69712a1c30ffa4310ce6a016cebf8021d09a7
Heather Booker <heather.booker@hotmail.com>
2015-07-10 16:36:39 -0700

    working with parse

--- scripts/controllers/addClothing-controller.js
+++ scripts/controllers/addClothing-controller.js
@@ -2,6 +2,67 @@ $("select").select2({
     ".mbl": 'dropdown-inverse'
 });
 
-//15 calls to create new clothing
+Parse.initialize("qmqVorzxIpQRkEbanvb8hczUA0PgxF3CVaDeUGJt", "bXUQxAnUNjdnkeV5GUyuwp5hY0yOL6bCFH3V98X1");
 
-var shirt = new Clothing('myshirt', 'pants', 'salsa', 'pink', '../assets/images/blueShirt.jpeg');
+var clothes = Parse.Object.extend("clothes");
+
+
+function makeNewClothes(imageFile) {
+
+    var articleOfClothing = new clothes();
+
+    articleOfClothing.set("color", "");
+    articleOfClothing.set("type", "");
+    articleOfClothing.set("img", imageFile);
+    articleOfClothing.set("use", "");
+    articleOfClothing.set("keywords", "");
+
+    articleOfClothing.save(null, {
+        success: function(item) {
+            //Success Callback 
+            console.log('New article of clothing successfully saved to data storage (parse)!')
+        },
+        error: function(gameScore, error) {
+            //Failure Callback
+            console.log('unable to save new article to parse')
+        }
+    });
+
+};
+
+
+function changeClothesImage(id) {
+
+    var fileUploadControl = $("#imageFileInput")[0];
+
+    if (fileUploadControl.files.length > 0) {
+        var file = fileUploadControl.files[0];
+        var name = file.name;
+
+        var parseFile = new Parse.File(name, file);
+
+        changeParseImage(id, parseFile);
+    }
+
+}
+
+function changeParseImage(id, image) {
+
+    var query = new Parse.Query(clothes);
+
+    query.get(id, {
+
+        success: function(object) {
+            console.log('woot parse object successfully retrieved!');
+            object.set('img', image)
+            object.save();
+        },
+
+        error: function(object, error) {
+            console.log('oh boogers parse object not retrieved: ' + JSON.stringify(error));
+        }
+
+        //     });
+        // }
+    })
+}
--- scripts/controllers/browseClothing-controller.js
+++ scripts/controllers/browseClothing-controller.js
@@ -1,28 +1,9 @@
-var clothes = [];
+var clothingArray = [];
 
 Parse.initialize("qmqVorzxIpQRkEbanvb8hczUA0PgxF3CVaDeUGJt", "bXUQxAnUNjdnkeV5GUyuwp5hY0yOL6bCFH3V98X1");
 
-function updateView(clothingArray) {
+var clothes = Parse.Object.extend("clothes");
 
-    for (var i = 0; i < clothingArray.length; i++) {
-
-        displayClothing(clothingArray[i], i)
-
-    }
-
-}
-
-
-function makeClothingArray(jsonArray) {
-
-    for (var i = 0; i < jsonArray.length; i++) {
-
-        var c = new Clothing(jsonArray[i].keywords, jsonArray[i].type, jsonArray[i].use, jsonArray[i].color, '../assets/images/' + i + '.jpg');
-        clothes.push(c);
-
-    }
-
-}
 
 
 function displayClothing(clothingObject, div) {
@@ -35,165 +16,53 @@ function displayClothing(clothingObject, div) {
 
 }
 
-function search(searchTerms) {
 
-}
+function makeClothingArray() {
 
-///
-
-
-function getFile() {
-
-    var fileUploadControl = $("#imageFileInput")[0];
-
-    if (fileUploadControl.files.length > 0) {
-        var file = fileUploadControl.files[0];
-        var name = "1.jpg";
-
-        var parseFile = new Parse.File(name, file);
-
-        updateParseImage('LfCOZ5I5id', parseFile);
-    }
-
-}
-
-function retrieveParseObject(id) {
-
-    var clothes = Parse.Object.extend("clothes");
     var query = new Parse.Query(clothes);
 
-    query.get('"' + id + '"', {
+    query.exists("img");
+    query.find({
 
-        success: function(object) {
-            console.log('woot parse object successfully retrieved!');
-            object.set('img')
+        success: function(articles) {
+            console.log('woot parse objects successfully retrieved!');
+            makeArrayFromParseJSON(articles);
         },
 
         error: function(object, error) {
-            console.log('oh boogers parse object not retrieved: ' + error);
+            console.log('oh boogers parse objects not retrieved: ' + error.code + error.message);
         }
 
     });
 }
 
-function updateParseImage(objectId, image) {
-    retrieveParseObject(objectId);
+function makeArrayFromParseJSON(JSONofArticlesFromParse) {
 
-    success: function(item) {
-        //Once the object is returned, we update its property and save it.
-        item.set('img', imageForParse);
-        item.save();
-    },
-
-    error: function(object, error) {
-        alert("Error when updating item: " + error.code + " " + error.message);
+    for (var i = 0; i < JSONofArticlesFromParse.length; i++) {
+        createArticleFromParse(JSONofArticlesFromParse, i);
     }
+
+    console.log(clothingArray);
 }
 
-// function newClothingObjectToParse(imageForParse) {
+function createArticleFromParse(arrayOfArticles, iteration) {
 
-//     var query = new Parse.Query(clothes);
+    var originalAttributes = arrayOfArticles[iteration].attributes;
 
-//     query.get('LfCOZ5I5id'), {
+    var imgParseURL = originalAttributes.img._url;
+    delete originalAttributes.img;
+    var idAttribute = arrayOfArticles[iteration].id;
 
-//         success: function(item) {
-//             //Once the object is returned, we update its property and save it.
-//             item.set('img', imageForParse);
-//             item.save();
-//         },
+    originalAttributes.parseId = idAttribute;
+    originalAttributes.imgURL = imgParseURL;
 
-//         error: function(object, error) {
-//             alert("Error when updating item: " + error.code + " " + error.message);
-//         }
+    clothingArray.push(originalAttributes);
 
-//     };
-// }
-
-//     imageForParse.save().then(function() {
-//         // The file has been saved to Parse.
-//         console.log('yay pictur saved');
-//     }, function(error) {
-//         // The file either could not be read, or could not be saved to Parse.
-//         console.log('whyyyyy');
-//     });
-
-//     objectToUpdate = "LfCOZ5I5id";
-
-//     objectToUpdate.set("img", imageForParse);
-// }
-
-//     //Extend the native Parse.Object class.
-//     var clothes = Parse.Object.extend("clothes");
-
-//     //Instantiate an object of the ListItem class
-//     var articleOfClothing = new clothes();
-
-//     //listItem is now the object that we want to save, so we assign the properties that we want on it.
-//     articleOfClothing.set("color", "blue");
-//     articleOfClothing.set("type", "dress");
-//     articleOfClothing.set("img", imageForParse);
-//     articleOfClothing.set("use", "salsa");
-//     articleOfClothing.set("keywords", "new");
-
-//     //We call the save method, and pass in success and failure callback functions.
-//     articleOfClothing.save(null, {
-//         success: function(item) {
-//             //Success Callback 
-//         },
-//         error: function(gameScore, error) {
-//             //Failure Callback
-//         }
-//     });
-// };
-
-// var parseFile = new Parse.File("photoOfThing", ../../assets/images/16.jpg);
-
-// parseFile.save().then(function() {
-//   // The file has been saved to Parse.
-//   console.log('yay pictur saved');
-// }, function(error) {
-//   // The file either could not be read, or could not be saved to Parse.
-//   console.log('whyyyyy');
-// });
-
-// var jobApplication = new Parse.Object("JobApplication");
-// jobApplication.set("applicantName", "Joe Smith");
-// jobApplication.set("applicantResumeFile", parseFile);
-// jobApplication.save();
-
-
-
-
-
-
-
+}
 
 $(document).ready(function() {
 
+    makeClothingArray();
 
-    $.ajax({
-
-        type: "get",
-        dataType: "json",
-        url: "../scripts/models/clothes.json",
-
-        beforeSend: function(request) {
-            request.overrideMimeType("application/json")
-        },
-
-        success: function(data) {
-
-            var clothesArray = data.results;
-            makeClothingArray(clothesArray);
-            updateView(clothes);
-            newClothingObjectToParse();
-            console.log('yay! you called json!');
-        },
-
-        error: function(err) {
-            console.log("uh oh spaghettis " + err.responseText);
-        }
-
-    })
 
 });
