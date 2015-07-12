6c538363b3117c909f8eb04e27fcd59c9a3384f8
Heather Booker <heather.booker@hotmail.com>
2015-07-11 18:42:07 -0700

    improving clothesmanager

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,14 @@ var clothesManager = new ClothesManager;
 
 //test all functions
 
-clothesManager.getFullArray;
+//clothesManager.getFullArray;
+
+//clothesManager.saveArticle;
+
+clothesManager.addArticle('hat', 'shirt', 'snip', 'boot');
+
+clothesManager.deleteArticle('YZVmkG18fi');
+
+clothesManager.updateArticle('YZVmkG18fi', 'color', 'purple');
+
+//clothesManager.makeQueryToParse;
